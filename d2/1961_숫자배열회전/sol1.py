52201da69363a75c222a6b6236f4d30fa2eaffe9
chanin shim <chanin.shim@gmail.com>
2021-02-28 21:54:53 +0900

    21.02.28 알고리즘 학습내용

--- d2/1961_숫자배열회전/sol1.py
+++ d2/1961_숫자배열회전/sol1.py
@@ -3,11 +3,24 @@ sys.stdin = open("input.txt")
 
 T = int(input())
 
-
-
-
-
 for tc in range(1, T+1):
+
+    N = int(input())
+
+    my_list = []
+    for _ in range(N):
+        my_list.append(list(map(int,input().split())))
+    # print(my_list)
+    # [
+    # [1, 2, 3],
+    # [4, 5, 6],
+    # [7, 8, 9]
+    # ]
+
+
+
+    # 너무 어려워 모르겠다.
+    # 규칙성 자체가 이해가 안감.
 
 
 
